cc938189d723da7bf74f59ba3c276886ea9070dd
Esther Tsai <esttsai@microsoft.com>
2025-06-25 16:30:56 +0800

    Fix CodeQL alert
    - Suppress CodeQL alert for webSecurity as the electron app is designed for local use.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -24,7 +24,7 @@ async function createWindow() {
     if (process.env.ELECTRON_START_URL) {
         windowOptions.webPreferences = {
             nodeIntegration: true,
-            webSecurity: false,
+            webSecurity: false, // CodeQL [SM02732] This tool is standalone and designed to run locally, so disabling web security is acceptable.
         };
     }
 
